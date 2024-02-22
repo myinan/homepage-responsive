b6176b1088f8b3e4711bf167581eebd91976ece4
myinan <m.yasir.inan@gmail.com>
2024-02-22 19:44:48 +0300

    Implement tablet header section

--- src/index.js
+++ src/index.js
@@ -1,1 +1,2 @@
 import "./styleDesktop";
+import "./styleTablet";
---
+++ src/styleTablet.js
@@ -0,0 +1,6 @@
+import "./styleTablet.css";
+
+import placeholderImgSmall from "./assets/placeholder-small.jpg";
+
+const tabletImg = document.querySelector(".aboutme-container .photo-tablet");
+tabletImg.src = placeholderImgSmall;
